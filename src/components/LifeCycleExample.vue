6c5a15c332b034d289fd5f7810bff74c888b8456
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-08-09 18:49:47 -0600

    Finished the knowledge check

---
+++ src/components/LifeCycleExample.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        beforeCreate () {
+            console.log(`currently in before create`);
+        },
+
+        created () {
+            console.log(`currently in created`);
+        },
+
+        beforeMount () {
+            console.log(`currently in before mount`);
+        },
+
+        mounted () {
+            console.log(`currently in mounted`);
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
